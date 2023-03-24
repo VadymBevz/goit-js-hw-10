1acccfdcc8b718768c032b310c0ed954d8a8f1c9
VadymBevz <vbevzv@gmail.com>
2023-03-24 12:38:07 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -17,7 +17,8 @@
 //       })}
 
 const URL = 'https://restcountries.com/v3.1/name/'
+
 export function fetchCountries(name){
-return fetch('${URL}/${name}?fields=name,capital,population,flags,languages')
-.then(res => res.json());
+  return fetch(`${URL}/${name}?fields=name,capital, population,flags,languages`)
+  .then(res => res.json());
 }
